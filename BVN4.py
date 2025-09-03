45b8bdebd8c23d87f38166d1a5bd167566b23552
kieutrinh030105 <kieutrinh030105@gmail.com>
2025-09-03 19:54:00 +0700

    Cập nhật code BVN4.py

--- BVN4.py
+++ BVN4.py
@@ -1,18 +1,18 @@
 def caesar_encrypt(text, k):
     result = ""
     for char in text:
-        if char.isalpha():  # chỉ mã hóa chữ cái
-            # Giữ nguyên chữ hoa / chữ thường
+        if char.isalpha(): 
             start = ord('A') if char.isupper() else ord('a')
             result += chr((ord(char) - start + k) % 26 + start)
         else:
-            # Giữ nguyên ký tự không phải chữ cái (dấu cách, dấu tiếng Việt...)
             result += char
     return result
 
+
 k = 11
-plaintext = "Võ Thị Kiều Trinh"
+plaintext = "Vo Thi Kieu Trinh"
 
 ciphertext = caesar_encrypt(plaintext, k)
-print("Plaintext: ", plaintext)
+
+print("Plaintext : ", plaintext)
 print("Ciphertext:", ciphertext)
